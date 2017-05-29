e0d3e3640cad26e18f5570e82ea54fd6a73a7e15
rand0me <not.randome@gmail.com>
2017-05-29 14:23:32 +0300

    fix(packages): fix typescript build
    affects: @sell/core, @sell/preact-renderer, @sell/triplelift

---
+++ packages/core/src/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Sell = (function () {
+    function Sell() {
+    }
+    return Sell;
+}());
+exports.Sell = Sell;
+//# sourceMappingURL=index.js.map
